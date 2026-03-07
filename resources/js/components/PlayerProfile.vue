08a6afd88dd3619b00dc5a5625b06bef23c93358
Michael Pardon <michael.pardon@e2estudios.com>
2026-03-07 23:44:52 +0000

    UI Fixes

--- resources/js/components/PlayerProfile.vue
+++ resources/js/components/PlayerProfile.vue
@@ -80,7 +80,7 @@
         <div v-if="profile.recent_achievements && profile.recent_achievements.length" class="modal-achievements">
           <h3 class="modal-section-title">Recent Achievements</h3>
           <div v-for="ach in profile.recent_achievements" :key="ach.id" class="ach-item">
-            <span class="ach-icon">{{ ach.icon || '&#127942;' }}</span>
+            <span class="ach-icon">{{ iconMap[ach.icon] || '\u{1F3C6}' }}</span>
             <div>
               <span class="ach-name">{{ ach.name }}</span>
               <span class="ach-desc">{{ ach.description }}</span>
@@ -115,6 +115,29 @@ export default {
       loading: true,
       error: null,
       confirmRemove: false,
+      iconMap: {
+        trophy: '\u{1F3C6}',
+        shield: '\u{1F6E1}',
+        crown: '\u{1F451}',
+        flame: '\u{1F525}',
+        lightning: '\u{26A1}',
+        star: '\u{2B50}',
+        swords: '\u{2694}',
+        crossed_swords: '\u{2694}',
+        scroll: '\u{1F4DC}',
+        arrow_up: '\u{2B06}',
+        diamond: '\u{1F48E}',
+        book: '\u{1F4D6}',
+        wizard: '\u{1F9D9}',
+        castle: '\u{1F3F0}',
+        people: '\u{1F465}',
+        person: '\u{1F9D1}',
+        calendar: '\u{1F4C5}',
+        handshake: '\u{1F91D}',
+        globe: '\u{1F30D}',
+        muscle: '\u{1F4AA}',
+        sparkles: '\u{2728}',
+      },
     };
   },
   computed: {
